f3aa89265eab467dc57afb2f06a3e6bfdc9a8a30
welloworld <36184466+welloworld@users.noreply.github.com>
2020-03-29 19:34:52 +0300

    Update SMT_build.sh

--- SMT_build.sh
+++ SMT_build.sh
@@ -1,3 +1,4 @@
+sudo apt-get install make gcc python2.7 scapy python-tk
 cd Firewall && make clean > /dev/null 2>&1 && make > /dev/null 2>&1 && ln -sf $PWD/fw.ko ../GUI/fw.ko
 cd ../Syscall_Hooking_Manager && make clean > /dev/null 2>&1 && make > /dev/null 2>&1 && ln -sf $PWD/shm.ko ../GUI/shm.ko
 cd ../Logger && ln -sf $PWD/Logger.py ../GUI/Logger.py
